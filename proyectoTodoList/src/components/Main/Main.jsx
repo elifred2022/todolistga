b9f71bd0407fff576f08610d0d11bb0cba56221f
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-11-27 00:21:21 -0300

    Mejora Componente Tarea, cambiando background

--- proyectoTodoList/src/components/Main/Main.jsx
+++ proyectoTodoList/src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import Tarea from './Tarea'
 const Main =()=>{
     return( 
     <div className="contenedorMain">
-        <img src="../public/img/hoja.jpg" alt="imagen de hoja rayada" />
+        
         <Tarea/>
     </div>
     )}
